cbb7cef38d3d6ace7838772f5dd67e23d6a514f8
Karl Haakonsen <cityhawk@pobox.com>
2023-10-20 16:25:27 -0400

    try calling convert

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
-from forex_python.converter import CurrencyRates
+from datetime import datetime
+from forex_python.converter import CurrencyRates, CurrencyCodes
 
 app = Flask(__name__)
 
@@ -16,10 +17,26 @@ def display_results():
     from_currency = request.form["from_currency"]
     to_currency = request.form["to_currency"]
     amount = float(request.form["amount_to_convert"])
-    #converted_amount = curr_rates.convert(from_currency,to_currency,amount)
 
-    converted_amount = amount * 2; # for now
-    print(converted_amount)
+    print(f"from_currency = {from_currency}")
+    print(f"to_currency = {to_currency}")
+    print(f"amount_currency = {amount}")
+
+    ccodes = CurrencyCodes()
+    ccode_display = ccodes.get_symbol(to_currency)
+    #converted_amount = amount * 2; # for now
+    #curr_rates.get_rate(from_currency, to_currency)
+    # this has not been working... so try it with a date/time:
+    date_obj =  datetime(2020, 5, 17) #arbitrary time in the past
+    print(f"Date chosen: {date_obj.month}/{date_obj.day}/{date_obj.year}")
+    try:
+        converted_amount = curr_rates.convert(from_currency,to_currency,amount, date_obj)
+    except Exception as e:
+       # By this way we can know about the type of error occurring
+        print("The error is: ",e)
+        converted_amount = 0.0;
+    #print(converted_amount)
 
 
-    return render_template("results.html", converted_amount=converted_amount)
+    return render_template("results.html", code=ccode_display, converted_amount=converted_amount)
+
